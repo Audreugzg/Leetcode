d535008b715f69819ccb9dacd0e643b532de2dbe
Zuge Gao <audreygaozu@hotmail.com>
2022-08-26 03:23:45 +0000

    Leetcode Q1

---
+++ twoSum.js
@@ -0,0 +1,9 @@
+var twoSum = function(nums,target){
+  const hash = {};
+  for(let i=0; i < nums.length; i++){
+    if(hash[nums[i]]>= 0){
+      return [hash[nums[i]],i];
+    }
+    hash[target-nums[i]]= i;
+  }
+}
