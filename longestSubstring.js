d3ce90502a95c30ef4ea7e77b43bab316cec3331
Audrey <apple@76:7c:7b:c4:3f:f4.hitronhub.home>
2022-09-12 21:06:43 -0700

    Q3 Longest Substring Without Repeating Characters

---
+++ longestSubstring.js
@@ -0,0 +1,26 @@
+var lengthOfLongestSubstring = function(s) {
+  if (!s) return 0;
+  if (s.length < 2) return s.length;
+  let longestString = s[0];
+  let currentString = s[0];
+  for (let i = 0; i < s.length; i++) {
+    let index = currentString.indexOf(s[i]);
+    if (index > -1) {
+      if(currentString.length > longestString.length){
+        longestString = currentString;
+
+      }
+      currentString = currentString.substring(index+1,currentString.length) + s[i];
+
+    }else{
+      currentString += s[i];
+    }
+    
+  }
+  if(currentString.length > longestString.length){
+    longestString = currentString;
+
+  }
+  return longestString.length;
+
+  };
